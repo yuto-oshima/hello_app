80dfc889368eca363105db2a6ea3a2ed2c65dd3d
yuto-oshima <oshima1998@gmail.com>
2020-06-24 22:34:43 +0900

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+
+  protect_from_forgery with: :exception
+
+  def hello
+
+    render html: '¡hola, mundo!'
+  end
+
+  def goodbye
+
+    render html: 'goodbye, world!'
+  end
+
+end
